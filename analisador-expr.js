4aa262150e548885b25c39e5f44ba17522a72695
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2020-10-30 18:02:42 +0000

    (23/10) Finalização Pilha

--- analisador-expr.js
+++ analisador-expr.js
@@ -8,7 +8,9 @@
 
  let analisador = new Stack()
 
- let expr = '6 + {8 / [3 - (9 *2)] + (4 * 3)}'
+ let expr = '6 + {8 / [3 - (9 * 2)] + (4 *3)}'
+//let expr = '6 + {8 / [3 - (9 *2)] + (4 * 3)} + (2-'
+ let info
 
  for (let i = 0; i <expr.length; i++){
      switch(expr.charAt(i)){
@@ -31,7 +33,42 @@
                 console.log('Erro: chave fechando na posição ' + i + 'não tem abertura correspondente')
             }
             break
+
+        case ']':
+            info = analisador.pop()
+            //info n pode ser vazio e seu tipo deve ser CO
+            if(info && info.tipo == 'CO'){
+                console.log('Colchete aberta na posição ' + info.pos + 'e fechada na posição ' + i)
+            }
+            else {
+                console.log('Erro: colchete fechando na posição ' + i + 'não tem abertura correspondente')
+            }
+            break
+        case ')':
+             info = analisador.pop()
+            //info n pode ser vazio e seu tipo deve ser PA
+            if(info && info.tipo == 'PA'){
+                console.log('parentese aberta na posição ' + info.pos + 'e fechada na posição ' + i)
+            }
+            else {
+                console.log('Erro: parentese fechando na posição ' + i + 'não tem abertura correspondente')
+            }
      }
  }
 
+ //analisar residuo da pilha 
+ while(pilha.size() > 0){ // enquanto ainda houver elementos na pilha
+     info = pilha.pop()
+     switch(info.tipo){
+         case 'PA':
+             console.log('Erro: Parentese aberto na posição' + info.pos + 'Não tem fechamento correspondente')
+            break
+        case 'CO':
+             console.log('Erro: colchete aberto na posição' + info.pos + 'Não tem fechamento correspondente')
+            break
+        case 'CH':
+             console.log('Erro: Chave aberto na posição' + info.pos + 'Não tem fechamento correspondente')
+            break             
+     }
+ }
  console.log(analisador)
---
+++ pilha3.js
@@ -0,0 +1,20 @@
+const Stack = require('./lib/Stack2')
+
+let pilha = new Stack()
+
+console.log(pilha)
+
+pilha.push(46)
+pilha.push(37)
+pilha.push(98)
+
+console.log(pilha)
+
+console.log('------------------')
+
+let x = pilha.pop()
+console.log(pilha)
+console.log('x:', x)
+
+pilha.pop()
+console.log(pilha)
